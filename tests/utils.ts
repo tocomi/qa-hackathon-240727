ca6094df9c2f9329f78802abc3646f541aa27856
tocomi <tocomi0112@gmail.com>
2024-07-27 14:14:42 +0900

    test: create new todo

--- tests/utils.ts
+++ tests/utils.ts
@@ -4,17 +4,17 @@ const BASE_URL = 'https://todo-app-qajp.vercel.app';
 const USER_EMAIL = 'test@hackaton.com';
 const USER_PASSWORD = 'test123!';
 
-export const signIn = async (page: Page) => {
+export const signIn = async (page: Page, email = USER_EMAIL) => {
   await test.step('sign in', async () => {
     await page.goto(BASE_URL);
 
     await page.getByRole('button', { name: 'Sign In' }).click();
-    await page.getByPlaceholder('name@example.com').fill(USER_EMAIL);
+    await page.getByPlaceholder('name@example.com').fill(email);
     await page.locator('#password').fill(USER_PASSWORD);
     await page.getByRole('button', { name: 'Log in with Email' }).click();
 
     await expect(page).toHaveURL('https://todo-app-qajp.vercel.app/todo');
-    await expect(page.getByRole('heading', { name: USER_EMAIL })).toBeVisible();
+    await expect(page.getByRole('heading', { name: email })).toBeVisible();
   });
 };
 
@@ -25,5 +25,18 @@ export const signOut = async (page: Page) => {
 
     await expect(page).toHaveURL(/^https:\/\/todo-app-qajp.vercel.app\/login/);
     await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible();
+  });
+};
+
+export const signUp = async (page: Page, email: string) => {
+  await test.step('sign up', async () => {
+    await page.goto(`${BASE_URL}/register`);
+
+    await page.getByPlaceholder('name@example.com').fill(email);
+    await page.locator('#password').fill(USER_PASSWORD);
+
+    await page.getByRole('button', { name: 'Sign up' }).click();
+
+    await expect(page).toHaveURL('https://todo-app-qajp.vercel.app/todo');
   });
 };
