c9f0a0ad8a494bae82510b72e0fd362dd2ebb975
tocomi <tocomi0112@gmail.com>
2024-07-27 14:45:03 +0900

    test: 更新系テスト

--- tests/edit.spec.ts
+++ tests/edit.spec.ts
@@ -28,11 +28,59 @@ test('更新系のテスト', async ({ page }) => {
     await page.getByRole('gridcell', { name: '28' }).click();
     await page.getByText('Due Date:').click();
 
-    await page.getByLabel('Active,').click();
-    await page.getByLabel('Block', { exact: true }).getByText('Block').click();
     await page.getByRole('button', { name: 'Create' }).click();
 
     await expect(page.getByText('New Todo')).toBeVisible();
+    await expect(page.getByText('New Description')).toBeVisible();
+    await expect(page.getByRole('gridcell', { name: 'ACTIVE' })).toBeVisible();
   });
-  // await signOut(page);
+
+  // TODO: ここで待たないと編集ボタンでダイアログが開かなくなる。できればやめたい
+  await page.waitForTimeout(500);
+
+  await test.step('6. Complete to-do', async () => {
+    await page.getByLabel('Edit Todo').click();
+    await page.getByLabel('Active,').click();
+    await page
+      .getByLabel('Completed', { exact: true })
+      .getByText('Completed')
+      .click();
+    await page.getByRole('button', { name: 'Update' }).click();
+    await expect(
+      page.getByRole('gridcell', { name: 'COMPLETED' })
+    ).toBeVisible();
+  });
+
+  await page.waitForTimeout(500);
+
+  await test.step('7. Edit to-do', async () => {
+    await page.getByLabel('Edit Todo').click();
+
+    await page.getByLabel('Title', { exact: true }).fill('Edited Todo');
+
+    await page.getByLabel('Description').click();
+    await page.getByLabel('Description').fill('Edited Description');
+
+    await page.getByRole('button', { name: /28/ }).click();
+    await page.getByRole('gridcell', { name: '27' }).click();
+    await page.getByText('Due Date:').click();
+
+    await page.getByLabel('Completed,').click();
+    await page.getByLabel('Block', { exact: true }).getByText('Block').click();
+
+    await page.getByRole('button', { name: 'Update' }).click();
+
+    await expect(page.getByText('Edited Todo')).toBeVisible();
+    await expect(page.getByText('Edited Description')).toBeVisible();
+    await expect(page.getByRole('gridcell', { name: 'BLOCK' })).toBeVisible();
+  });
+
+  await test.step('8. Delete to-do', async () => {
+    await page.getByLabel('Delete Todo').click();
+    await page.getByLabel('Confirm').click();
+
+    await expect(page.getByText('Edited Todo')).not.toBeVisible();
+  });
+
+  await signOut(page);
 });
