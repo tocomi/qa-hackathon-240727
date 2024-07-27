f7fdc2be87a3532cb67f3f63fd35150fd517068f
kenshin <103884604+homemade-ramen@users.noreply.github.com>
2024-07-27 13:35:12 +0900

    test: Sign out (#4)
    * add sign out
    
    * fix
    
    * remove break

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,11 +4,24 @@ const BASE_URL = 'https://todo-app-qajp.vercel.app';
 const USER_EMAIL = 'test@hackaton.com'
 const USER_PASSWORD = 'test123!'
 
-test('sign in', async ({ page }) => {
+test('認証', async ({ page }) => {
   await page.goto(BASE_URL);
-  await page.getByRole('button', { name: 'Sign In' }).click();
-  await page.getByPlaceholder('name@example.com').fill(USER_EMAIL);
-  await page.locator('#password').fill(USER_PASSWORD);
-  await page.getByRole('button', { name: 'Log in with Email' }).click();
-  await expect(page.getByRole('heading', { name: USER_EMAIL })).toBeVisible()
+  
+  await test.step('sign in', async () => {
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.getByPlaceholder('name@example.com').fill(USER_EMAIL);
+    await page.locator('#password').fill(USER_PASSWORD);
+    await page.getByRole('button', { name: 'Log in with Email' }).click();
+
+    await expect(page).toHaveURL("https://todo-app-qajp.vercel.app/todo")
+    await expect(page.getByRole('heading', { name: USER_EMAIL })).toBeVisible()
+  })
+
+  await test.step('sign out', async () => {
+    await page.getByRole('menuitem', { name: 'Todos' }).click();
+    await page.getByRole('menuitem', { name: 'Signout ⌘Q' }).click();
+
+    await expect(page).toHaveURL(/^https:\/\/todo-app-qajp.vercel.app\/login/)
+    await expect(page.getByRole('heading', { name: 'Sign In' })).toBeVisible()
+  })
 });
